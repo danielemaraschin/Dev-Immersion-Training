0e71a25bca21eabed39e95d8bb1307c5e5efeb76
danielemaraschin <danikmaraschin@gmail.com>
2021-09-15 08:59:13 +0200

    class1

---
+++ aula1/index.js
@@ -0,0 +1,19 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+
+
+var nome = "Dani";
+console.log("Bem vindo " + nome);
+
+var notaPrimeiroBimestre = 9.5;
+var notaSegundoBimestre = 5.5;
+var notaTerceiroBimestre = 7.5;
+var notaQuartoBimestre = 6.6;
+
+var notaFinal =
+  (notaPrimeiroBimestre +
+    notaSegundoBimestre +
+    notaTerceiroBimestre +
+    notaQuartoBimestre) /
+  4;
+
+var NotaArredondada = notaFinal.toFixed(1);
