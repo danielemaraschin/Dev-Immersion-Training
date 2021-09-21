0d4a18b93dea80d56faacad343f886eb65c2ba8b
danielemaraschin <danikmaraschin@gmail.com>
2021-09-21 20:47:24 +0200

    inner HTML

--- Day5/aluraflix.js
+++ Day5/aluraflix.js
@@ -1,8 +1,12 @@
 function adicionarFilme() {
 
+    console.log('clicado')
     var campoFilmeFavorito = document.getElementById("filme").value;
     var elementoFilmeFavorito = "<img src=" + campoFilmeFavorito + ">";
+    console.log(elementoFilmeFavorito);
     var elementoListaFilmes = document.getElementById("listaFilmes");
-    elementoListaFilmes.innerHtml =  elementoListaFilmes.innerHtml + elementoFilmeFavorito;
-//vai add o elementoFilmeFavorito dentro do elementoListaFilmes, no HTML.
+    console.log(elementoListaFilmes);
+    elementoListaFilmes.innerHtml = elementoListaFilmes.innerHtml + elementoFilmeFavorito;
+    document.getElementById("filme").value = "";
+    //vai add o elementoFilmeFavorito dentro do elementoListaFilmes, no HTML.
 }
