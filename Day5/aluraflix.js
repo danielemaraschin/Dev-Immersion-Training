bf7bf63fc4a3900af3d482d532b1e1272fbe5ea6
danielemaraschin <danikmaraschin@gmail.com>
2021-09-21 20:27:24 +0200

    Add new itens in a list

--- Day5/aluraflix.js
+++ Day5/aluraflix.js
@@ -1,5 +1,9 @@
 function adicionarFilme() {
-        
+
     var campoFilmeFavorito = document.getElementById("filme").value;
-    console.log(campoFilmeFavorito)
+    var elementoFilmeFavorito = "<img src=" + campoFilmeFavorito + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes")
+    elementoListaFilmes.innerHtml =  elementoListaFilmes.innerHtml + elementoFilmeFavorito
+//vai add o elementoFilmeFavorito dentro do elementoListaFilmes, no HTML.
+
 }
