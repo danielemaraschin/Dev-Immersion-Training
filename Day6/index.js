afd730221ecf85a89fdb83ad59762ea91d843d5a
danielemaraschin <danikmaraschin@gmail.com>
2021-09-22 17:49:12 +0200

    working with object

---
+++ Day6/index.js
@@ -0,0 +1,17 @@
+var rafa = {
+    nome: "Rafa",
+    vitorias: 2,
+    empates: 1,
+    derrotas: 1,
+    pontos: 0
+}
+
+var paulo = {
+    nome: 'Paulo',
+    vitorias: 1,
+    empates: 1,
+    derrotas: 2,
+    pontos: 0
+}
+
+console.log(rafa)
