7d1d568d989a9d1a3b7dd183b3f4bac09fdc3124
danielemaraschin <danikmaraschin@gmail.com>
2021-09-22 17:31:26 +0200

    Working with params and scope

--- Day5/aluraflix.js
+++ Day5/aluraflix.js
@@ -1,18 +1,13 @@
 function adicionarFilme() {
-
     var filmeFavorito = document.getElementById("filme").value;
-
-    if(filmeFavorito.endsWith(".jpg")){
-        var elementofilmeFavorito = "<img src=" + filmeFavorito + ">"; 
-        var elementoListaFilmes = document.getElementById("listaFilmes");
-       
-        elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementofilmeFavorito;
-        //vai add o elementoFilmeFavorito dentro do elementoListaFilmes, no HTML
-    }else{
-        console.error("Adicione uma img")
-        //fica vermelho no formato de erro.
-    }
-
+    listarFilmeNaTela(filmeFavorito);
     document.getElementById("filme").value = "";
     //independente se mandar certo ou nao a imagem, vai limpar o campo.
+}
+
+function listarFilmeNaTela(filme) {
+    var elementoFilmeFavorito = "<img src=" + filme + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+    //vai add o elementoFilmeFavorito dentro do elementoListaFilmes, no HTML
 }
