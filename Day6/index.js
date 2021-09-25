dca27adca561cecbce7029c9d453f6f7508cbe49
danielemaraschin <danikmaraschin@gmail.com>
2021-09-25 09:38:13 +0200

    working with list and for

--- Day6/index.js
+++ Day6/index.js
@@ -1,46 +1,36 @@
-var rafa = {
-    nome: "Rafa",
-    vitorias: 2,
-    empates: 1,
-    derrotas: 1,
-    pontos: 0
-}
+var rafa = {nome: "Rafa", vitorias: 2, empates: 1, derrotas: 1,  pontos: 0 };
 
-var paulo = {
-    nome: 'Paulo',
-    vitorias: 1,
-    empates: 1,
-    derrotas: 2,
-    pontos: 0
-}
+var paulo = { nome: "Paulo", vitorias: 1, empates: 1, derrotas: 2, pontos: 0};
 
-calculaPontos(rafa);
 
 function calculaPontos(jogador){
-    var pontos = (jogador.vitorias * 3) + jogador.empates;
+    var pontos = jogador.vitorias * 3 + jogador.empates;
     return pontos;
 }
 
 rafa.pontos = calculaPontos(rafa);
-paulo.pontos = calculaPontos(paulo)
+paulo.pontos = calculaPontos(paulo);
+//gui.pontos = calculaPontos(gui)
 
 var jogadores = [rafa, paulo];
+console.log(jogadores)
 
 function exibeJogadoresTela (jogadores) {
-    var elemento = "";
-    for (var i = 0; i< jogadores.length; i++){
-        elemento += "<tr> <td>" + jogadores[i].nome + "</td>";
+    var elemento = ""
+    for (var i = 0; i < jogadores.length; i++) {
+        elemento += "<tr><td>" + jogadores[i].nome + "</td>";
         elemento += "<td>" + jogadores[i].vitorias + "</td>";
         elemento += "<td>" + jogadores[i].empates + "</td>";
         elemento += "<td>" + jogadores[i].derrotas +  "</td>";
         elemento += "<td>" + jogadores[i].pontos +  "</td>";
-        elemento += "<td><button onClick='adicionarVitoria("+ i+ ")'>Vitórias</button></td>";
-        elemento += "<td><button onClick='adicionarEmpate()'>Empates</button></td>";
-        elemento += "<td><button onClick='adicionarDerrota()'>Derrotas</button></td>";
+        elemento += "<td><button onClick='adicionarVitoria("+ i+ ")'>Vitória</button></td>";
+        elemento += "<td><button onClick='adicionarEmpate()'>Empate</button></td>";
+        elemento += "<td><button onClick='adicionarDerrota()'>Derrota</button></td>";
         elemento += "</tr>";
-
     }
+    console.log(elemento)
     var tabelaJogadores = document.getElementById("tabelaJogadores");
+    console.log(tabelaJogadores)
     tabelaJogadores.innerHTML = elemento;
 }
-exibeJogadoresTela(jogadores)
+exibeJogadoresTela(jogadores);
