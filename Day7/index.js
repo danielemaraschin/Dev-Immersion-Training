46eb1f03f045d96a5825e72e01225f49defaf9f0
danielemaraschin <danikmaraschin@gmail.com>
2021-09-26 10:49:36 +0200

    initializing a variable

--- Day7/index.js
+++ Day7/index.js
@@ -1,5 +1,12 @@
 var carta1 = {nome: "Bulbassauro", atributos: {ataque: 7, defesa: 8, magia: 6 }};
+var carta2 = {nome: "Mewtwo", atributos: {ataque: 9, defesa: 7, magia: 9 }};
+var carta3 = {nome: "Psyduck", atributos: {ataque: 5, defesa: 7, magia: 10 }};
 
-console.log(carta1)
-console.log(carta1.atributos)
-console.log(carta1.atributos.magia)
+console.log(cartas)
+
+var cartaMaquina 
+var cartaJogador = 0
+
+
+var cartas = [carta1, carta2, carta3];
+console.log(cartas)
