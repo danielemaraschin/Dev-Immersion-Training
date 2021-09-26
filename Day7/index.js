fff8723423a8a9b445cf4179d1b8cc6dea606dc0
danielemaraschin <danikmaraschin@gmail.com>
2021-09-26 16:53:46 +0200

    working with parseInt and math.random

--- Day7/index.js
+++ Day7/index.js
@@ -2,11 +2,18 @@ var carta1 = {nome: "Bulbassauro", atributos: {ataque: 7, defesa: 8, magia: 6 }}
 var carta2 = {nome: "Mewtwo", atributos: {ataque: 9, defesa: 7, magia: 9 }};
 var carta3 = {nome: "Psyduck", atributos: {ataque: 5, defesa: 7, magia: 10 }};
 
-console.log(cartas)
 
 var cartaMaquina 
 var cartaJogador = 0
 
 
 var cartas = [carta1, carta2, carta3];
-console.log(cartas)
+
+
+function sortearCarta(){
+  var numeroCartaMaquina = parseInt(Math.random() * 3)
+
+  cartaMaquina = cartas[numeroCartaMaquina]
+  console.log(cartaMaquina)
+  }
+  
